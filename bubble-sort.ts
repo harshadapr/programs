5d3ad74e6400d7b8f6528be17ed1bdc221a53ff0
Harshada <67410367+harshadapr@users.noreply.github.com>
2023-11-04 00:44:36 +0530

    Create bubble-sort.ts

---
+++ bubble-sort.ts
@@ -0,0 +1,18 @@
+// bubble-sort.ts
+
+function bubbleSort(arr: number[]): number[] {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        const temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+const unsortedArray = [64, 34, 25, 12, 22, 11, 90];
+const sortedArray = bubbleSort(unsortedArray);
+console.log(sortedArray);
