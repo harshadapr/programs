a22ff17771b2c66ece9c1d50024351b4e31cd4bf
Harshada <67410367+harshadapr@users.noreply.github.com>
2023-12-04 17:52:48 +0530

    Create variadic2.go

---
+++ variadic2.go
@@ -0,0 +1,18 @@
+
+
+package main
+
+import "fmt"
+
+func main() {
+	
+	variadicExample()
+	variadicExample("red", "blue")
+	variadicExample("red", "blue", "green")
+	variadicExample("red", "blue", "green", "yellow")
+}
+
+func variadicExample(s ...string) {
+	fmt.Println(s)
+}
+
